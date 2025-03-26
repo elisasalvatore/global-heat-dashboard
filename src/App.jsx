4abca578865e7fe6af80ed3995e9dcbec5588044
Elisa <elisa.salvatore110@gmail.com>
2025-03-26 16:48:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="p-6 w-[100vw] h-[100vh] bg-blue-900 flex justify-start items-start">
+			<h1 className="text-white text-6xl">Global Heat</h1>
+		</div>
+	);
+}
+
+export default App;
